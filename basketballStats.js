3534cc063dad19a8ed93ed3e786821039ecffb18
Loriah Pope <llp260@nyu.edu>
2014-09-29 09:55:43 -0400

    Update basketballStats.js
    final commit from

--- basketballStats.js
+++ basketballStats.js
@@ -1,125 +1,107 @@
-var data = JSON.stringify([{"position": "F", "freeThrowsAttempted": 9, "defensiveRebounds": 9, "offensiveRebounds": 1, "threesMade": 3, "turnovers": 1, "freeThrowsMade": 8, "assists": 5, "threesAttempted": 9, "fieldGoalsMade": 10, "fieldGoalsAttempted": 21, "name": "L. James", "steals": 0, "team": "Heat"}, {"position": "F", "freeThrowsAttempted": 0, "defensiveRebounds": 2, "offensiveRebounds": 0, "threesMade": 1, "turnovers": 0, "freeThrowsMade": 0, "assists": 1, "threesAttempted": 2, "fieldGoalsMade": 1, "fieldGoalsAttempted": 2, "name": "R. Lewis", "steals": 0, "team": "Heat"}, {"position": "C", "freeThrowsAttempted": 2, "defensiveRebounds": 7, "offensiveRebounds": 0, "threesMade": 0, "turnovers": 0, "freeThrowsMade": 1, "assists": 2, "threesAttempted": 5, "fieldGoalsMade": 6, "fieldGoalsAttempted": 14, "name": "C. Bosh", "steals": 1, "team": "Heat"}, {"position": "G", "freeThrowsAttempted": 4, "defensiveRebounds": 3, "offensiveRebounds": 0, "threesMade": 1, "turnovers": 3, "freeThrowsMade": 2, "assists": 1, "threesAttempted": 2, "fieldGoalsMade": 4, "fieldGoalsAttempted": 12, "name": "D. Wade", "steals": 0, "team": "Heat"}, {"position": "G", "freeThrowsAttempted": 2, "defensiveRebounds": 4, "offensiveRebounds": 1, "threesMade": 1, "turnovers": 4, "freeThrowsMade": 2, "assists": 2, "threesAttempted": 3, "fieldGoalsMade": 1, "fieldGoalsAttempted": 8, "name": "R. Allen", "steals": 1, "team": "Heat"}, {"position": "F", "freeThrowsAttempted": 0, "defensiveRebounds": 0, "offensiveRebounds": 0, "threesMade": 0, "turnovers": 1, "freeThrowsMade": 0, "assists": 0, "threesAttempted": 0, "fieldGoalsMade": 0, "fieldGoalsAttempted": 0, "name": "S. Battier", "steals": 1, "team": "Heat"}, {"position": "C", "freeThrowsAttempted": 0, "defensiveRebounds": 5, "offensiveRebounds": 1, "threesMade": 0, "turnovers": 0, "freeThrowsMade": 0, "assists": 0, "threesAttempted": 0, "fieldGoalsMade": 0, "fieldGoalsAttempted": 1, "name": "C. Andersen", "steals": 1, "team": "Heat"}, {"position": "G", "freeThrowsAttempted": 2, "defensiveRebounds": 1, "offensiveRebounds": 0, "threesMade": 0, "turnovers": 0, "freeThrowsMade": 2, "assists": 1, "threesAttempted": 1, "fieldGoalsMade": 0, "fieldGoalsAttempted": 2, "name": "N. Cole", "steals": 0, "team": "Heat"}, {"position": "F", "freeThrowsAttempted": 0, "defensiveRebounds": 1, "offensiveRebounds": 0, "threesMade": 0, "turnovers": 0, "freeThrowsMade": 0, "assists": 0, "threesAttempted": 0, "fieldGoalsMade": 1, "fieldGoalsAttempted": 2, "name": "U. Haslem", "steals": 0, "team": "Heat"}, {"position": "F", "freeThrowsAttempted": 3, "defensiveRebounds": 1, "offensiveRebounds": 2, "threesMade": 0, "turnovers": 1, "freeThrowsMade": 1, "assists": 1, "threesAttempted": 0, "fieldGoalsMade": 4, "fieldGoalsAttempted": 7, "name": "M. Beasley", "steals": 0, "team": "Heat"}, {"position": "G", "freeThrowsAttempted": 5, "defensiveRebounds": 1, "offensiveRebounds": 0, "threesMade": 0, "turnovers": 0, "freeThrowsMade": 4, "assists": 0, "threesAttempted": 0, "fieldGoalsMade": 2, "fieldGoalsAttempted": 3, "name": "M. Chalmers", "steals": 1, "team": "Heat"}, {"position": "F", "freeThrowsAttempted": 0, "defensiveRebounds": 1, "offensiveRebounds": 0, "threesMade": 0, "turnovers": 0, "freeThrowsMade": 0, "assists": 0, "threesAttempted": 1, "fieldGoalsMade": 0, "fieldGoalsAttempted": 1, "name": "J. Jones", "steals": 0, "team": "Heat"}, {"position": "G", "freeThrowsAttempted": 0, "defensiveRebounds": 1, "offensiveRebounds": 0, "threesMade": 1, "turnovers": 0, "freeThrowsMade": 0, "assists": 1, "threesAttempted": 2, "fieldGoalsMade": 1, "fieldGoalsAttempted": 2, "name": "T. Douglas", "steals": 0, "team": "Heat"}, {"position": "F", "freeThrowsAttempted": 6, "defensiveRebounds": 8, "offensiveRebounds": 2, "threesMade": 3, "turnovers": 3, "freeThrowsMade": 5, "assists": 2, "threesAttempted": 4, "fieldGoalsMade": 7, "fieldGoalsAttempted": 10, "name": "K. Leonard", "steals": 1, "team": "Spurs"}, {"position": "F", "freeThrowsAttempted": 6, "defensiveRebounds": 6, "offensiveRebounds": 2, "threesMade": 0, "turnovers": 0, "freeThrowsMade": 4, "assists": 2, "threesAttempted": 0, "fieldGoalsMade": 5, "fieldGoalsAttempted": 10, "name": "T. Duncan", "steals": 0, "team": "Spurs"}, {"position": "C", "freeThrowsAttempted": 0, "defensiveRebounds": 8, "offensiveRebounds": 1, "threesMade": 1, "turnovers": 2, "freeThrowsMade": 0, "assists": 6, "threesAttempted": 3, "fieldGoalsMade": 2, "fieldGoalsAttempted": 7, "name": "B. Diaw", "steals": 1, "team": "Spurs"}, {"position": "G", "freeThrowsAttempted": 0, "defensiveRebounds": 2, "offensiveRebounds": 0, "threesMade": 0, "turnovers": 1, "freeThrowsMade": 0, "assists": 2, "threesAttempted": 3, "fieldGoalsMade": 0, "fieldGoalsAttempted": 5, "name": "D. Green", "steals": 2, "team": "Spurs"}, {"position": "G", "freeThrowsAttempted": 2, "defensiveRebounds": 1, "offensiveRebounds": 0, "threesMade": 0, "turnovers": 0, "freeThrowsMade": 2, "assists": 2, "threesAttempted": 1, "fieldGoalsMade": 7, "fieldGoalsAttempted": 18, "name": "T. Parker", "steals": 0, "team": "Spurs"}, {"position": "G", "freeThrowsAttempted": 5, "defensiveRebounds": 4, "offensiveRebounds": 0, "threesMade": 3, "turnovers": 2, "freeThrowsMade": 4, "assists": 4, "threesAttempted": 6, "fieldGoalsMade": 6, "fieldGoalsAttempted": 11, "name": "M. Ginobili", "steals": 0, "team": "Spurs"}, {"position": "C", "freeThrowsAttempted": 2, "defensiveRebounds": 2, "offensiveRebounds": 0, "threesMade": 0, "turnovers": 0, "freeThrowsMade": 1, "assists": 2, "threesAttempted": 0, "fieldGoalsMade": 1, "fieldGoalsAttempted": 1, "name": "T. Splitter", "steals": 1, "team": "Spurs"}, {"position": "G", "freeThrowsAttempted": 0, "defensiveRebounds": 1, "offensiveRebounds": 0, "threesMade": 5, "turnovers": 0, "freeThrowsMade": 0, "assists": 2, "threesAttempted": 8, "fieldGoalsMade": 6, "fieldGoalsAttempted": 10, "name": "P. Mills", "steals": 0, "team": "Spurs"}, {"position": "F", "freeThrowsAttempted": 0, "defensiveRebounds": 0, "offensiveRebounds": 0, "threesMade": 0, "turnovers": 0, "freeThrowsMade": 0, "assists": 2, "threesAttempted": 0, "fieldGoalsMade": 0, "fieldGoalsAttempted": 0, "name": "M. Bonner", "steals": 0, "team": "Spurs"}, {"position": "G", "freeThrowsAttempted": 0, "defensiveRebounds": 2, "offensiveRebounds": 0, "threesMade": 0, "turnovers": 0, "freeThrowsMade": 0, "assists": 1, "threesAttempted": 0, "fieldGoalsMade": 2, "fieldGoalsAttempted": 3, "name": "M. Belinelli", "steals": 0, "team": "Spurs"}, {"position": "C", "freeThrowsAttempted": 0, "defensiveRebounds": 0, "offensiveRebounds": 0, "threesMade": 0, "turnovers": 0, "freeThrowsMade": 0, "assists": 0, "threesAttempted": 0, "fieldGoalsMade": 1, "fieldGoalsAttempted": 1, "name": "J. Ayres", "steals": 0, "team": "Spurs"}, {"position": "G", "freeThrowsAttempted": 0, "defensiveRebounds": 0, "offensiveRebounds": 0, "threesMade": 0, "turnovers": 0, "freeThrowsMade": 0, "assists": 0, "threesAttempted": 1, "fieldGoalsMade": 0, "fieldGoalsAttempted": 2, "name": "C. Joseph", "steals": 0, "team": "Spurs"}, {"position": "F", "freeThrowsAttempted": 2, "defensiveRebounds": 0, "offensiveRebounds": 1, "threesMade": 0, "turnovers": 0, "freeThrowsMade": 2, "assists": 0, "threesAttempted": 0, "fieldGoalsMade": 0, "fieldGoalsAttempted": 0, "name": "A. Baynes", "steals": 0, "team": "Spurs"} ]);
+var request = require('request');
 
-var players = JSON.parse(data);
+var urls = ['http://foureyes.github.io/csci-ua.0480-fall2014-002/homework/02/2014-06-15-heat-spurs.json', 'http://foureyes.github.io/csci-ua.0480-fall2014-002/homework/02/2014-04-09-thunder-clippers.json'];
 
-var heat = [];
-var spurs = [];
+urls.forEach(function(url){
+		request(url, function (error, response, body) {
+		var players = JSON.parse(body);
+		basketball(players);
+	});
+});
 
-function heatOrSpurs(players){
+function basketball(players){
 
-	for(var i = 0; i < players.length; i++){
-		if(players[i].team === "Heat"){
-			heat.push(players[i]);
-		} else{
-			spurs.push(players[i]);
-		}
-	}
-	return heat;
-	return spurs;
-}
+	var first = players[0].team;
 
-heatOrSpurs(players);
+	var team1 = players.filter(function(person){
+		return person.team === first
+	});
 
-function calculateScore(team){
-	var score = 0;
-	for(var i = 0; i < team.length; i++){
-		var threes = team[i].threesMade * 3;
-		var twos = (team[i].fieldGoalsMade - team[i].threesMade) * 2;
-		var ones = team[i].freeThrowsMade;
-
-		score = score + threes + twos + ones;
-	}
-
-	return score;
-}
-
-function percentageFromThrees(players){
-	var highestPercentage = 0;
-	var highestPlayer = "";
-	for(var i = 0; i < players.length; i++){
-		var player = players[i].name;
-		/*var threes = players[i].threesMade * 3;
-		var twos = (players[i].fieldGoalsMade - players[i].threesMade) * 2;
-		var ones = players[i].freeThrowsMade;*/
-		var totalPoints = (players[i].threesMade * 3) + ((players[i].fieldGoalsMade - players[i].threesMade) * 2) + (players[i].freeThrowsMade);
-		var threePercent = (Math.floor(((players[i].threesMade * 3)/totalPoints) * 100));
-
-		if (totalPoints > 10 && threePercent > highestPercentage){
-			highestPercentage = threePercent;
-			highestPlayer = player;
-		}
-	}
-	return highestPlayer;
-}
-
-
-
-function mostRebounds(team1, team2){
 	var team1Name = team1[0].team;
-	var team1Rebounds = 0;
+
+	var team2 = players.filter(function(person){
+		return person.team !== first
+	});
 
 	var team2Name = team2[0].team;
-	var team2Rebounds = 0;
 
-	for(var i = 0; i < team1.length; i++){
-		team1Rebounds = team1Rebounds + team1[i].defensiveRebounds + team1[i].offensiveRebounds;
+	console.log("\n" + team1Name + " vs. " + team2Name + "\n");
+
+	function calculateScore(team){
+		var score = 0;
+		team.forEach(function(player){
+			score = score + (player.threesMade * 3) + ((player.fieldGoalsMade - player.threesMade) * 2) + (player.freeThrowsMade); 
+		});
+		return score;
 	}
 
-	for(var i = 0; i < team1.length; i++){
-		team2Rebounds = team2Rebounds + team2[i].defensiveRebounds + team2[i].offensiveRebounds;
+	function percentFromThrees() {
+		var highestPercentage = 0;
+		var highestPlayer = "";
+
+		players.forEach(function(player){
+			var totalPoints = (player.threesMade * 3) + ((player.fieldGoalsMade - player.threesMade) * 2) + (player.freeThrowsMade);
+			var threePercent = (Math.floor(((player.threesMade * 3)/totalPoints) * 100));
+
+			if (totalPoints > 10 && threePercent > highestPercentage){
+				highestPercentage = threePercent;
+				highestPlayer = player.name;
+			}
+		});
+		return highestPlayer;
 	}
 
-	if(team1Rebounds > team2Rebounds){
-		return team1Name + " with " + team1Rebounds;
-	}else{
-		return team2Name + " with " + team2Rebounds;
-	}
-}
+	function mostRebounds(team1, team2){
 
-function mostAssists(players){
-	var highestAssists = 0;
-	var highestPlayer = "";
-	for(var i = 0; i < players.length; i++){
-		if(players[i].position != "G"){
-			var player = players[i].name;
-			var assists = players[i].assists;
+		var team1Rebounds = 0;
+
+		var team2Rebounds = 0;
+
+		for(var i = 0; i < team1.length; i++){
+			team1Rebounds = team1Rebounds + team1[i].defensiveRebounds + team1[i].offensiveRebounds;
+		}
+
+		for(var i = 0; i < team1.length; i++){
+			team2Rebounds = team2Rebounds + team2[i].defensiveRebounds + team2[i].offensiveRebounds;
+		}
+
+		if(team1Rebounds > team2Rebounds){
+			return team1Name + " with " + team1Rebounds;
+		}else{
+			return team2Name + " with " + team2Rebounds;
+		}
+	}
+
+	function mostAssists(){
+		var nonGuard = players.filter(function(person){
+			return person.position !== "G";
+		});
+
+		var highestAssists = 0;
+		var highestPlayer = "";
+
+		nonGuard.map(function(person) {
+			var player = person.name;
+			var assists = person.assists;
 
 			if(assists > highestAssists){
 				highestAssists = assists;
 				highestPlayer = player;
 			}
-		}
+
+		});
+		return highestPlayer + " with " + highestAssists;
 	}
-	return highestPlayer + " with " + highestAssists;
-}
 
-function turnoversVAssusts(players){
-	var moreTurnovers = [];
-	for(var i = 0; i < players.length; i++){
-		if(players[i].turnovers > players[i].assists){
-			moreTurnovers.push(players[i].name);
+	console.log("Final Score: " + team1Name + ": " + calculateScore(team1) + ", " + team2Name + ": " + calculateScore(team2) + "\n =====");
+	console.log("* Player with highest percentage of points from three pointers: " + percentFromThrees(players));
+	console.log("* Team with most rebounds: " + mostRebounds(team1, team2));
+	console.log("* Non guard player with most assists: " + mostAssists());
+	console.log("* Players with more turnovers than assists: ");
+	var moreTurnovers = players.filter(function(person){
+		if(person.turnovers > person.assists){
+			console.log("\t" + person.name);
 		}
-	}
-	return moreTurnovers;
+	});
+
 }
-
-
-
-
-
-console.log("Final Score: Heat: " + calculateScore(heat) + ", Spurs: " + calculateScore(spurs) + "\n =====");
-console.log("* Player with highest percentage of points from three pointers: " + percentageFromThrees(players));
-console.log("* Team with most rebounds: " + mostRebounds(heat, spurs));
-console.log("* Non guard player with most assists: " + mostAssists(players));
-console.log("* Players with more turnovers than assists: \n" + turnoversVAssusts(players));
-
-
-
-
-
-
-
-
-
-
-
